d5519a05e70828de42beda104ba1eb6a8455def2
FernandoUs <fernando.usurin@utec.edu.pe>
2023-05-02 18:07:22 -0500

    Agregando el modelo Client

--- serverproject.py
+++ serverproject.py
@@ -7,7 +7,17 @@ from flask import (
 )
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
+import uuid;
 #Config 
 dev=Flask(__name__)
 dev.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:1234//postgres@localhost:5432/project'
+db= SQLAlchemy(dev)
+
+#Models
+class Clients(db.Model):
+    __tablename__ = 'clients'
+    id= db.Column(db.String(7), primary_key=True, default=lambda: str(uuid.uuid4()), server_default=db.text("uuid_generate_v4()") )
+    firstname= db.Column(db.String(30), nullable=False)
+    lastname= db.Column(db.String(50), nullable=False, unique=False)
+    email= db.Column(db.String(60), nullable=False, unique=True)
+    foto= db.Colum
