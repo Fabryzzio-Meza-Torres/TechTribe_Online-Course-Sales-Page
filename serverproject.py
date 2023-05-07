ccb818cae209ec2e4cc3468e7c128ffe9af97594
Fabryzzio-Meza-Torres <fabryzziomt@gmail.com>
2023-05-06 23:20:44 -0500

    Actualización de modelos
    versión 1.0.1

--- serverproject.py
+++ serverproject.py
@@ -7,80 +7,93 @@ from flask import (
 )
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-import uuid;
+import uuid
+import os
+from datetime import datetime,timedelta
+import sys
 #Config 
 dev=Flask(__name__)
 dev.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost:5432/project'
 db= SQLAlchemy(dev)
+migrate = Migrate(dev, db)
 
 #Models
 class Clients(db.Model):
-    __tablename__ = 'Clients'
-    id = db.Column(db.String(7), primary_key=True, default=lambda: str(uuid.uuid4()), server_default=db.text("uuid_generate_v4()") )
+    __tablename__ = 'clients'
+    id = db.Column(db.String(10), primary_key=True, default=lambda: str(uuid.uuid4()), server_default=db.text("uuid_generate_v4()") )
     firstname= db.Column(db.String(30), nullable=False)
     lastname= db.Column(db.String(50), nullable=False, unique=False)
     email= db.Column(db.String(60), nullable=False, unique=True)
-    foto= db.Column(db.String(500), nullable=True)
+    created_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=db.text("now()"))
+    modified_at = db.Column(db.DateTime(timezone=True), nullable=True, server_default=db.text("now()"))
     
-    def __init__(self, firstname, lastname, email, foto):
+    def __init__(self, firstname, lastname, email):
         self.firstname= firstname
         self.lastname= lastname
         self.email= email
-        self.foto= foto
+        self.modified_at = datetime.utcnow()
+        self.created_at = datetime.utcnow()
 
 class Trabajadores(db.Model):
     __tablename__ = 'trabajadores'
-    id = db.Column(db.String(7), nullable=False, default=lambda: str(uuid.uuid4()), server_default=db.text("uuid_generate_v4()"))
-    firstname = db.Column(db.String(30), nullable=False)
-    lastname = db.Column(db.String(50), nullable=False, unique=True)
+    id = db.Column(db.String(10),primary_key=True, nullable=False, default=lambda: str(uuid.uuid4()), server_default=db.text("uuid_generate_v4()"))
+    firstname = db.Column(db.String(30), nullable=False,unique=True)
+    lastname = db.Column(db.String(30), nullable=False, unique=True)
+    created_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=db.text("now()"))
+    modified_at = db.Column(db.DateTime(timezone=True), nullable=True, server_default=db.text("now()"))
 
-    def __init__(self, id, firstname, lastname):
-        self.id = id 
+    def __init__(self, firstname, lastname):
         self.firstname = firstname
         self.lastname = lastname
+        self.modified_at = datetime.utcnow()
+        self.created_at = datetime.utcnow()
     
 class Producto(db.Model):
-    __tablename__ = 'Producto'
-    id = db.Column(db.String(7), nullable=False, default=lambda: str(uuid.uuid4()), server_default=db.text("uuid_generate_v4()"))
-    nombre = db.Column(db.String(30), nullable=False)
-    precio = db.Column(db.Float, nullable=False)
-    descripcion = db.Column(db.String(500), nullable=False)
-    foto = db.Column(db.String(500), nullable=True)
-    cantidad = db.Column(db.Integer, nullable=False)
+    __tablename__ = 'products'
+    id = db.Column(db.String(10), nullable=False, default=lambda: str(uuid.uuid4()), server_default=db.text("uuid_generate_v4()"))
+    name = db.Column(db.String(30), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.String(500), nullable=False)
 
-    def __init__(self, nombre, precio, descripcion, foto, cantidad):
+
+    def __init__(self, nombre, precio, descripcion):
         self.nombre = nombre
         self.precio = precio
         self.descripcion = descripcion
-        self.foto = foto
-        self.cantidad = cantidad
+
 
 class Tarjeta(db.Model):
-    __tablename__ = "tarjeta"
-    numero_tarjeta = db.Column(db.String(18), nullable=False)
-    fecha_vencimiento = db.Column(db.DateTime.date(2050,12,31), nullable=False)
-    clave = db.Column(db.Integer(3), nullable=False)
-    id_cliente = db.Column(db.Integer(8), nullable=False)
-    dni = db.Column(db.Integer(8), nullable=False)
-    banco_emisor = db.Column(db.String(30), nullable=False)
+    __tablename__ = "credit_card"
+    id = db.Column(db.String(10), primary_key=True, default=lambda: str(uuid.uuid4()), server_default=db.text("uuid_generate_v4()"))
+    creditcard_number = db.Column(db.String(18), nullable=False)
+    expiration_date= db.Column(db.Interval, nullable=False)
+    password = db.Column(db.String(30), nullable=False)
+    id_client = db.Column(db.String(10),db.ForeignKey('clients.id'), nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=db.text("now()"))
+    modified_at = db.Column(db.DateTime(timezone=True), nullable=True, server_default=db.text("now()"))
 
-    def __init__(self, numero_tarjeta, fecha_vencimiento,clave,id_cliente, dni, banco_emisor):
-        self.numero_tarjeta = numero_tarjeta
-        self.fecha_vencimiento = fecha_vencimiento
-        self.clave = clave
-        self.dni = dni 
-        self.banco_emisor = banco_emisor
+    def __init__(self, creditcard_number, password, id_client):
+        self.creditcard_number = creditcard_number
+        self.expiration_date = timedelta(days=30,weeks=0,months=0,year=4) # rango de 4 años y un mes desde hoy
+        self.password = password
+        self.id_client = id_client
+        self.modified_at = datetime.utcnow()
+        self.created_at = datetime.utcnow()
+        
 
-class Carrito(db.Model):
-    __tablename__ = 'carrito'
-    id_carrito = db.Column(db.String(20),nullable=False)
-    nombre_producto = db.Column(db.String(30), nullable=False)
-    precio_total = db.Column(db.Integer(10000), nullable=False)
+class Orden_de_Compra(db.Model):
+    __tablename__ = 'purchase_order'
+    id= db.Column(db.String(10), primary_key=True, default=lambda: str(uuid.uuid4()), server_default=db.text("uuid_generate_v4()"))
+    product_name = db.Column(db.String(30), nullable=False)
+    total_price = db.Column(db.Integer(10000), nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=db.text("now()"))
+    modified_at = db.Column(db.DateTime(timezone=True), nullable=True, server_default=db.text("now()"))
 
-    def __init__(self, id_carrito, nombre_producto, precio_total):
-        self.id_carrito = id_carrito
-        self.nombre_producto = nombre_producto
-        self.precio_total = precio_total
+    def __init__(self, product_name,total_price):
+        self.product_name = product_name
+        self.total_price = total_price
+        self.modified_at = datetime.utcnow()
+        self.created_at = datetime.utcnow()
         
 
 
