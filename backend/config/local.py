a3c039c1bb0901c80e64720f76ff0042fa655454
Fabryzzio-Meza-Torres <fabryzziomt@gmail.com>
2023-07-12 13:33:55 -0500

    Actualizacion vue

--- backend/config/local.py
+++ backend/config/local.py
@@ -1,3 +1,3 @@
 config = {
-    'DATABASE_URI': 'postgresql://postgres:1234@localhost:5432/project'
+    'DATABASE_URI': 'postgresql://postgres:mezatorres123@localhost:5432/project'
 }
--- backend/config/qa.py
+++ backend/config/qa.py
@@ -1,3 +1,3 @@
 config = {
-    'DATABASE_URI': 'postgresql://postgres:1234@localhost:5432/tests',
+    'DATABASE_URI': 'postgresql://postgres:mezatorres23@localhost:5432/tests',
 }
