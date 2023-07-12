e6d9c326eb8a9aa5efb4975d94bd88d93abd4338
FernandoUs <fernando.usurin@utec.edu.pe>
2023-07-12 01:17:49 -0500

    Ruta profesores

--- frontend/src/components/ContentBox.vue
+++ frontend/src/components/ContentBox.vue
@@ -23,44 +23,70 @@
       <a class="BContainer">Ver asesoría</a>
     </div>
   </div>
-  <div class="containerprof" v-else-if="currentRoute === 'profesores'">
-    <div id="worker_1" class="workers">
-      <div class="worker">
-        <span class="name">Marvin </span><span class="surname">Abisrror</span>
+  <div class="containerboxprof" v-else-if="currentRoute === 'profesores'">
+    <div
+      v-for="profesor in profesores"
+      :key="profesor.id"
+      class="item-profesor"
+    >
+      <div id="worker_1" class="workers">
+        <div class="worker">
+          <span class="name">{{ profesor.firstname }}</span
+          ><span class="surname">{{ profesor.lastname }}</span>
+        </div>
+      </div>
+      <a class="imgCompo">
+        <img :src="getProfesoresImageUrl(profesor.id)" alt="imgProfesor" />
+      </a>
+      <p>Contenido que dicta:</p>
+      <div
+        v-if="
+          getProductByProfesores(profesor.producto_id).length > 0 ||
+          getAsesoriaByProfesores(profesor.producto_id).length > 0
+        "
+      >
+        <p
+          v-for="curso in getProductByProfesores(profesor.producto_id)"
+          :key="curso.id"
+          class="cursoProfesor"
+        >
+          {{ curso.name }}
+        </p>
+        <p
+          v-for="asesoria in getAsesoriaByProfesores(profesor.producto_id)"
+          :key="asesoria.id"
+          class="cursoProfesor"
+        >
+          {{ asesoria.name }}
+        </p>
       </div>
     </div>
-    <a class="imgCompo">
-      <img
-        :src="require('../assets/Profesores/Marvin_Abisrror.png')"
-        alt="imgProfesor"
-      />
-    </a>
-    <p>
-      Ingeniero de Sistemas con Maestría en Ciencias de la Computación y
-      especialidad en Búsquedas Heurísticas.
-    </p>
   </div>
 </template>
 
 <script>
 import { getAllAsesorias } from "@/services/asesorias.api.js";
 import { getAllCursos } from "@/services/cursos.api.js";
+import { getAllProfesores } from "@/services/profesores.api.js";
 export default {
   name: "ContentBoxProduct",
   components: {},
   mounted() {
     this.loadCursos();
     this.loadAsesorias();
+    this.loadProfesores();
   },
   data() {
     return {
       cursos: [],
       asesorias: [],
+      profesores: [],
     };
   },
   created() {
     this.loadCursos();
     this.loadAsesorias();
+    this.loadProfesores();
     console.log(this.cursos);
   },
   methods: {
@@ -76,11 +102,28 @@ export default {
         this.asesorias = data.data.asesorias;
       }
     },
+    async loadProfesores() {
+      const data = await getAllProfesores();
+      if (data.data.success) {
+        this.profesores = data.data.profesores;
+      }
+    },
     getCursosImageUrl(cursoName) {
       return require(`@/assets/Cursos/${cursoName}.jpg`);
     },
     getAsesoriasImageUrl(asesoriaName) {
       return require(`@/assets/Asesorias/${asesoriaName}.jpg`);
+    },
+    getProfesoresImageUrl(profesorName) {
+      return require(`@/assets/Profesores/${profesorName}.png`);
+    },
+    getProductByProfesores(profesor_producto_id) {
+      return this.cursos.filter((curso) => curso.id === profesor_producto_id);
+    },
+    getAsesoriaByProfesores(profesor_producto_id) {
+      return this.asesorias.filter(
+        (asesoria) => asesoria.id === profesor_producto_id
+      );
     },
   },
   computed: {
@@ -95,6 +138,12 @@ export default {
   display: flex;
   flex-wrap: wrap;
   justify-content: left;
+}
+
+.containerboxprof {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
 }
 
 .item-product {
@@ -141,6 +190,28 @@ export default {
 }
 
 .containerprof {
+  margin: 25px;
+  height: 340px;
+  justify-content: right;
+  width: 275px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  flex-direction: column;
+  padding: 35px;
+  border-radius: 8px;
+  background: linear-gradient(to bottom, #2c3e50, #1a252f);
+  color: #fff;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 1.5;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.3);
+  transition: transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
+}
+
+.item-profesor {
   margin: 25px;
   height: 340px;
   width: 275px;
