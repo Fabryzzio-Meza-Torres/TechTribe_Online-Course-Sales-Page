af284100357b2eb3dfb43decabf6aacbae2c3f49
FernandoUs <fernando.usurin@utec.edu.pe>
2023-07-12 19:15:13 -0500

    Update client_controller.py

--- backend/app/client_controller.py
+++ backend/app/client_controller.py
@@ -78,4 +78,48 @@ def create_client():
     
 @clients_bp.route('/api/signin', methods=['POST'])
 def signin():
-    pass
+    error_list=[]
+    returned_code= 201
+    try:
+        body = request.get_json()
+
+        if 'email' not in body:
+            error_list.append('email is required')
+        else:
+            email = body.get('email')
+
+        if 'password' not in body:
+            error_list.append('password is required')
+        else:
+            password = body.get('password')
+
+        client_db= Clients.query.filter_by(email=email).first()
+
+        if client_db is None:
+            error_list.append('email does not exist')
+        else:
+            if client_db.password != password:
+                error_list.append('password is incorrect')
+        
+        if len(error_list) > 0:
+            returned_code = 400
+        else:
+            token = jwt.encode({'client_id': client_db.id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, config['SECRET_KEY'], config['ALGORITHM'])
+    except Exception as e:
+        print('e: ', e)
+        returned_code = 500
+
+    if returned_code == 400:
+        return jsonify({
+            'success': False,
+            'errors': error_list,
+            'message': 'Error signing in'
+        })
+    elif returned_code != 201:
+        abort(returned_code)
+    else:
+        return jsonify({
+            'success': True,
+            'token': token,
+            'client_id': client_db.id,
+        }), returned_code
